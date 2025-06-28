40e47ab49d8a0e60dc5f34e5c2b81fa6e69c4bc8
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-06-28 16:54:24 +0700

    revert code

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -17,7 +17,6 @@ from web_scraping.preprocessing import *
 import joblib, os, mysql.connector
 import pandas as pd
 from dotenv import load_dotenv
-import tempfile
 load_dotenv()
 
 
@@ -34,184 +33,171 @@ def run_scraping():
         helpful_model = joblib.load(os.path.join(PREDICT_HELPFUL_DIR, "model_helpfulness_final.pkl"))
 
         # Headless options
-        with tempfile.TemporaryDirectory() as tmp_profile:
-            options = Options()
-            options.binary_location = "/usr/bin/google-chrome"
-            options.add_argument('--headless=new')
-            options.add_argument('--no-sandbox')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-dev-shm-usage')
-            options.add_argument('--window-size=1920,1080')
-            options.add_argument(f"--user-data-dir={tmp_profile}")
+        options = Options()
+        options.binary_location = "/usr/bin/google-chrome"
+        options.add_argument('--headless=new')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--window-size=1920,1080')
 
-            service = Service("/usr/bin/chromedriver")
+        service = Service("/usr/bin/chromedriver")
+        driver = webdriver.Chrome(service=service, options=options)
+        # driver = webdriver.Chrome(options=options)
+        maps_url = f"https://www.google.com/maps/search/solo+safari/"
+        driver.get(maps_url)
 
-            driver = None
-            print('Starting Chrome driver...')
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Ulasan')]"))
+        ).click()
+
+        # Tunggu dan klik tombol urutkan
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Urutkan')]"))
+        ).click()
+
+        # Tunggu menu muncul dan klik "Terbaru"
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "action-menu"))
+        )
+        menu_button = driver.find_element(By.ID, "action-menu")
+        menu_button.find_element(By.CSS_SELECTOR, "[data-index='1']").click()
+
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "jJc9Ad"))
+        )
+
+        # Ambil timestamp terakhir dari DB
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM reviews ORDER BY id DESC LIMIT 1")
+        last_data = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if last_data:
+            target_timestamp = last_data['created_at'].replace(minute=0, second=0, microsecond=0)
+            last_username = last_data['username']
+        else:
+            target_timestamp = time_to_timestamp("1 hari lalu")
+            last_username = ""
+
+        before_timestamp = datetime.now()
+        on_target = False
+        frame = driver.find_element(By.CLASS_NAME, "DxyBCb")
+        scroll_origin = ScrollOrigin.from_element(frame, 0, 0)
+        target_found = False
+
+        # Scroll dan kumpulkan review
+        while not on_target:
+            while not target_found:
+                ActionChains(driver).scroll_from_origin(scroll_origin, 0, 1500).perform()
+                time_list = driver.find_elements(By.CLASS_NAME, "rsqaWe");
+                cleaned_time_list = []
+                for time in time_list:
+                    if "Diedit" in time.text:
+                        cleaned_time = time.text.replace("Diedit ", "")
+                        cleaned_time_list.append(cleaned_time)
+                    else:
+                        cleaned_time_list.append(time.text)
+                times = time_to_timestamp(cleaned_time_list)
+                if any(time < target_timestamp for time in times): target_found = True
+
+            reviews = driver.find_elements(By.CLASS_NAME, "jJc9Ad")
+            data_reviews = []
+
+            for review in reviews:
+                time_ = times[reviews.index(review)]
+                if time_ >= before_timestamp: continue
+                if time_ < target_timestamp: on_target = True; break
+                content = getReviewText(review)
+                if not content: continue  # skip if no content
+                raw_content = content
+                username = getUsername(review)
+                is_local_guide, reviewer_number_of_reviews = getSubUserInfo(review)
+                rating = getRating(review)
+                likes = getLikes(review)
+                image_count = getImageCount(review)
+                rc1, rc2, rc3, rc4 = getReviewContexts(review)
+                answer = getAnswer(review)
+                is_extreme = getIsExtremeReview(rating)
+
+                data_reviews.append({
+                    "username": username,
+                    "time": time_,
+                    "rating": rating,
+                    "content": content,
+                    "likes": likes,
+                    "review_context_1": rc1,
+                    "review_context_2": rc2,
+                    "review_context_3": rc3,
+                    "review_context_4": rc4,
+                    "answer": answer,
+                    "is_local_guide": is_local_guide,
+                    "reviewer_number_of_reviews": reviewer_number_of_reviews,
+                    "image_count": image_count,
+                    "is_extreme_review": is_extreme,
+                    "raw_content": raw_content
+                })
+
+        driver.quit()
+
+        # filter berdasarkan username terakhir
+        if last_username:
+            filtered = []
+            passed_last = False
+            for r in data_reviews:
+                if r['username'] == last_username:
+                    passed_last = True
+                    continue
+                if passed_last is False:
+                    filtered.append(r)
+            data_reviews = filtered
+
+        data_reviews.reverse()
+
+        # preprocessing dan fitur tambahan
+        for item in data_reviews:
+            item['content'] = preprocessing(item['content'])
+            item['answered_any_review_context'] = answer_context(
+                item['review_context_1'], item['review_context_2'],
+                item['review_context_3'], item['review_context_4']
+            )
+            item['contains_question'] = contains_question(item['content'])
+            item['contains_number'] = contains_number(item['content'])
+            item['review_length'] = get_length(item['content'])
+            item['is_weekend'] = is_weekend(item['time'])
+
             try:
-                driver = webdriver.Chrome(service=service, options=options)
-                # driver = webdriver.Chrome(options=options)
-                maps_url = f"https://www.google.com/maps/search/solo+safari/"
-                driver.get(maps_url)
+                helpful_data = pd.DataFrame([{
+                    'stars': item['rating'],
+                    'review_length': item['review_length'],
+                    'image_count': item['image_count'],
+                    'review_age_days': (datetime.now() - item['time']).days,
+                    'is_extreme_rating': item['is_extreme_review'],
+                    'is_weekend': item['is_weekend'],
+                    'reviewerNumberOfReviews': item['reviewer_number_of_reviews'],
+                }])
+                item['is_helpful'] = int(helpful_model.predict(helpful_data)[0])
+            except:
+                item['is_helpful'] = 0
 
-                print('Searching elements...')
-                WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Ulasan')]"))
-                ).click()
-
-                # Tunggu dan klik tombol urutkan
-                WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Urutkan')]"))
-                ).click()
-
-                # Tunggu menu muncul dan klik "Terbaru"
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.ID, "action-menu"))
-                )
-                menu_button = driver.find_element(By.ID, "action-menu")
-                menu_button.find_element(By.CSS_SELECTOR, "[data-index='1']").click()
-
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "jJc9Ad"))
-                )
-                print('Elements found, starting scraping...')
-
-                # Ambil timestamp terakhir dari DB
-                conn = mysql.connector.connect(
-                    host=os.getenv("DB_HOST"),
-                    database=os.getenv("DB_NAME"),
-                    user=os.getenv("DB_USER"),
-                    password=os.getenv("DB_PASSWORD")
-                )
-                cursor = conn.cursor(dictionary=True)
-                cursor.execute("SELECT * FROM reviews ORDER BY id DESC LIMIT 1")
-                last_data = cursor.fetchone()
-                cursor.close()
-                conn.close()
-
-                if last_data:
-                    target_timestamp = last_data['created_at'].replace(minute=0, second=0, microsecond=0)
-                    last_username = last_data['username']
-                else:
-                    target_timestamp = time_to_timestamp("1 hari lalu")
-                    last_username = ""
-
-                before_timestamp = datetime.now()
-                on_target = False
-                frame = driver.find_element(By.CLASS_NAME, "DxyBCb")
-                scroll_origin = ScrollOrigin.from_element(frame, 0, 0)
-                target_found = False
-                print('Scraping reviews...')
-
-                # Scroll dan kumpulkan review
-                while not on_target:
-                    while not target_found:
-                        ActionChains(driver).scroll_from_origin(scroll_origin, 0, 1500).perform()
-                        time_list = driver.find_elements(By.CLASS_NAME, "rsqaWe");
-                        cleaned_time_list = []
-                        for time in time_list:
-                            if "Diedit" in time.text:
-                                cleaned_time = time.text.replace("Diedit ", "")
-                                cleaned_time_list.append(cleaned_time)
-                            else:
-                                cleaned_time_list.append(time.text)
-                        times = time_to_timestamp(cleaned_time_list)
-                        if any(time < target_timestamp for time in times): target_found = True
-
-                    reviews = driver.find_elements(By.CLASS_NAME, "jJc9Ad")
-                    data_reviews = []
-
-                    for idx, review in enumerate(reviews):
-                        time_ = times[idx]
-                        if time_ >= before_timestamp: continue
-                        if time_ < target_timestamp: on_target = True; break
-                        content = getReviewText(review)
-                        if not content: continue  # skip if no content
-                        raw_content = content
-                        username = getUsername(review)
-                        is_local_guide, reviewer_number_of_reviews = getSubUserInfo(review)
-                        rating = getRating(review)
-                        likes = getLikes(review)
-                        image_count = getImageCount(review)
-                        rc1, rc2, rc3, rc4 = getReviewContexts(review)
-                        answer = getAnswer(review)
-                        is_extreme = getIsExtremeReview(rating)
-
-                        data_reviews.append({
-                            "username": username,
-                            "time": time_,
-                            "rating": rating,
-                            "content": content,
-                            "likes": likes,
-                            "review_context_1": rc1,
-                            "review_context_2": rc2,
-                            "review_context_3": rc3,
-                            "review_context_4": rc4,
-                            "answer": answer,
-                            "is_local_guide": is_local_guide,
-                            "reviewer_number_of_reviews": reviewer_number_of_reviews,
-                            "image_count": image_count,
-                            "is_extreme_review": is_extreme,
-                            "raw_content": raw_content
-                        })
-
-                print('Scraping completed, processing data...')
-
-                # filter berdasarkan username terakhir
-                if last_username:
-                    filtered = []
-                    passed_last = False
-                    for r in data_reviews:
-                        if r['username'] == last_username:
-                            passed_last = True
-                            continue
-                        if passed_last is False:
-                            filtered.append(r)
-                    data_reviews = filtered
-
-                data_reviews.reverse()
-
-                # preprocessing dan fitur tambahan
-                for item in data_reviews:
-                    item['content'] = preprocessing(item['content'])
-                    item['answered_any_review_context'] = answer_context(
-                        item['review_context_1'], item['review_context_2'],
-                        item['review_context_3'], item['review_context_4']
-                    )
-                    item['contains_question'] = contains_question(item['content'])
-                    item['contains_number'] = contains_number(item['content'])
-                    item['review_length'] = get_length(item['content'])
-                    item['is_weekend'] = is_weekend(item['time'])
-
-                    try:
-                        helpful_data = pd.DataFrame([{
-                            'stars': item['rating'],
-                            'review_length': item['review_length'],
-                            'image_count': item['image_count'],
-                            'review_age_days': (datetime.now() - item['time']).days,
-                            'is_extreme_rating': item['is_extreme_review'],
-                            'is_weekend': item['is_weekend'],
-                            'reviewerNumberOfReviews': item['reviewer_number_of_reviews'],
-                        }])
-                        item['is_helpful'] = int(helpful_model.predict(helpful_data)[0])
-                    except:
-                        item['is_helpful'] = 0
-
-                print('Saving data to database...')
-                if data_reviews:
-                    to_db(data_reviews)
-                    return {
-                        "status": "success",
-                        "total_reviews": len(data_reviews),
-                    }
-                else:
-                    return {
-                        "status": "error",
-                        "message": "Tidak ada review baru yang ditemukan.",
-                    }
-            finally:
-                if driver is not None:
-                    driver.quit()
+        if data_reviews:
+            to_db(data_reviews)
+            return {
+                "status": "success",
+                "total_reviews": len(data_reviews),
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "Tidak ada review baru yang ditemukan.",
+            }
 
     except Exception as e:
         return {"status": "error", "message": str(e)}
